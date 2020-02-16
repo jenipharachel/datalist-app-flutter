e1ca0e8329fda0557ad9d06f502721a769b7e0fb
Jenipha <jenipharachel@gmail.com>
2020-02-16 14:52:11 +0530

    code split into separate files

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) => MaterialApp(
-//         title: 'List Data App',
-//         debugShowCheckedModeBanner: false,
-//         theme: ThemeData(primaryColor: Colors.black, accentColor: Colors.black),
-//         home: Home(),
-//       );
-// }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'Isolate Demo of Data';
+    final appTitle = 'Bike Stations in NYC';
 
     return MaterialApp(
       title: appTitle,
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../repository/data_repo.dart';
 import '../models/data.dart';
-import '../widgets/data_tile.dart';
 import 'package:http/http.dart' as http;
 
 class MyHomePage extends StatelessWidget {
@@ -29,34 +28,20 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-// class Home extends StatefulWidget {
-//   @override
-//   _HomeState createState() => _HomeState();
-// }
+class DatasList extends StatelessWidget {
+  final List<Data> datas;
 
-// class _HomeState extends State<Home> {
-//   List<Data> _datas = <Data>[];
+  DatasList({Key key, this.datas}) : super(key: key);
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     listenForBeers();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) => Scaffold(
-//         appBar: AppBar(
-//           centerTitle: true,
-//           title: Text('Top Beers'),
-//         ),
-//         body: ListView.builder(
-//           itemCount: _beers.length,
-//           itemBuilder: (context, index) => BeerTile(_beers[index]),
-//         ),
-//       );
-
-//   void listenForBeers() async {
-//     final Stream<Data> stream = await getBeers();
-//     stream.listen((Beer beer) => setState(() => _beers.add(beer)));
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: datas.length,
+        itemBuilder: (context, index) {
+          return (ListTile(
+            title: Text(datas[index].stationName),
+            subtitle: Text(datas[index].statusValue),
+          ));
+        });
+  }
+}
